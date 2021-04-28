6e932b675111842d3dba67aed9c720b6f7bc5b16
ExErorr <58633014+ExErorr@users.noreply.github.com>
2021-04-28 19:21:58 +0200

    Aktualnosci Powiadomienia dodaj oraz uporzadkowanie modeli w kokpicie

---
+++ Kokpit/Controllers/AktualnosciPowiadomieniaController .cs
@@ -0,0 +1,16 @@
+﻿using Kokpit.Models;
+using Kokpit.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Shared.Models.Autoryzacja;
+namespace Kokpit.Controllers
+{
+    [RoutePrefix("api/logowanie")]
+    class AktualnosciPowiadomieniaController : ApiController
+    {
+    }
+}
