bf01a38e54372d031fd3b54a8ac966f72f33c00f
Marek Żubrycki <zubrycki.marek@gmail.com>
2021-05-23 16:20:50 +0200

    Ankieta init

---
+++ Formularze/Models/Ankiety/AnkietaModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formularze.Models.Ankiety
+{
+    public class AnkietaModel
+    {
+        public int IdAnkiety { get; set; }
+        public string Tresc { get; set; }
+        public DateTime DataOd { get; set; }
+        public DateTime DataDo { get; set; }
+        public bool CzyWlasneOdp { get; set; }
+        public DateTime DataUtworzenia { get; set; }
+        public string ImieNazwisko { get; set; }
+        public int IdWybranego { get; set; }
+        List<WyborModel> wybory { get; set; }
+    }
+}
